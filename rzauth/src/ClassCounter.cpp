309d02c8ffc0ff5d36fc3a3c684144038890b16e
glandu2 <glandu2epvp@gmail.com>
2013-12-13 23:43:17 +0100

    Add ClassCounter.cpp

---
+++ rzauth/src/ClassCounter.cpp
@@ -0,0 +1,31 @@
+#include "Network/ClassCounter.h"
+
+//generated with notepad++ with this pattern: (.*) ->
+//#include "\1.h"\ntemplate<> unsigned long ClassCounter<\1>::objectCount = 0;\ntemplate<> const unsigned int ClassCounter<\1>::classTypeHash = ClassCounter__internal_object_engine_class_counter++;\n
+/* Class list:
+ClientData
+ClientInfo
+DB_Account
+DesPasswordCipher
+ServerInfo
+*/
+
+#include "ClientData.h"
+template<> unsigned long ClassCounter<ClientData>::objectCount = 0;
+template<> const unsigned int ClassCounter<ClientData>::classTypeHash = ClassCounter__internal_object_engine_class_counter++;
+
+#include "ClientInfo.h"
+template<> unsigned long ClassCounter<ClientInfo>::objectCount = 0;
+template<> const unsigned int ClassCounter<ClientInfo>::classTypeHash = ClassCounter__internal_object_engine_class_counter++;
+
+#include "DB_Account.h"
+template<> unsigned long ClassCounter<DB_Account>::objectCount = 0;
+template<> const unsigned int ClassCounter<DB_Account>::classTypeHash = ClassCounter__internal_object_engine_class_counter++;
+
+#include "DesPasswordCipher.h"
+template<> unsigned long ClassCounter<DesPasswordCipher>::objectCount = 0;
+template<> const unsigned int ClassCounter<DesPasswordCipher>::classTypeHash = ClassCounter__internal_object_engine_class_counter++;
+
+#include "ServerInfo.h"
+template<> unsigned long ClassCounter<ServerInfo>::objectCount = 0;
+template<> const unsigned int ClassCounter<ServerInfo>::classTypeHash = ClassCounter__internal_object_engine_class_counter++;
