c6ade2dc641c29906a1cbc29708d861d88156231
glandu2 <glandu2epvp@gmail.com>
2013-12-23 21:51:15 +0100

    Change way to declare global config variable and their name binding to support msvc

--- rzauth/src/GlobalConfig.h
+++ rzauth/src/GlobalConfig.h
@@ -4,25 +4,37 @@
 #include "ConfigInfo.h"
 
 struct GlobalConfig {
-	struct {
-		std::string& driver = CFG("db.driver", "FreeTDS");
-		std::string& server = CFG("db.server", "127.0.0.1");
-		int& port = CFG("db.port", 1433);
-		std::string& name = CFG("db.name", "Auth");
-		std::string& account = CFG("db.account", "sa");
-		std::string& password = CFG("db.password", "");
-		std::string& salt = CFG("db.salt", "2012");
+	struct DbAccount {
+		std::string& driver, &server, &name, &account, &password, &salt;
+		int& port;
+
+		DbAccount() :
+			driver(CFG("db.driver", "FreeTDS")),
+			server(CFG("db.server", "127.0.0.1")),
+			name(CFG("db.name", "Auth")),
+			account(CFG("db.account", "sa")),
+			password(CFG("db.password", "")),
+			salt(CFG("db.salt", "2012")),
+			port(CFG("db.port", 1433)) {}
 	} dbAccount;
 
-	struct {
-		std::string& listenIp = CFG("listen.client.ip", "0.0.0.0");
-		int& port = CFG("listen.client.port", 4500);
-		std::string& desKey = CFG("listen.client.des_key", "MERONG");
+	struct ClientConfig {
+		std::string& listenIp, &desKey;
+		int& port;
+
+		ClientConfig() :
+			listenIp(CFG("listen.client.ip", "0.0.0.0")),
+			desKey(CFG("listen.client.des_key", "MERONG")),
+			port(CFG("listen.client.port", 4500)) {}
 	} clientConfig;
 
-	struct {
-		std::string& listenIp = CFG("listen.game.ip", "0.0.0.0");
-		int& port = CFG("listen.game.port", 4502);
+	struct GameConfig {
+		std::string& listenIp;
+		int& port;
+
+		GameConfig() :
+			listenIp(CFG("listen.game.ip", "0.0.0.0")),
+			port(CFG("listen.game.port", 4502)) {}
 	} gameConfig;
 
 	static GlobalConfig* get();
