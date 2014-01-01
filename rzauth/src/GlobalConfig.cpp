fcff6df06edc792bb3304fa20979eec975060be9
glandu2 <glandu2epvp@gmail.com>
2014-01-01 02:15:22 +0100

    Set default config to auth related names

--- rzauth/src/GlobalConfig.cpp
+++ rzauth/src/GlobalConfig.cpp
@@ -1,4 +1,5 @@
 #include "GlobalConfig.h"
+#include "RappelzLibConfig.h"
 
 GlobalConfig* GlobalConfig::get() {
 	static GlobalConfig config;
@@ -7,4 +8,8 @@ GlobalConfig* GlobalConfig::get() {
 
 void GlobalConfig::init() {
 	GlobalConfig::get();
+	RappelzLibConfig::get()->app.appName.setDefault("RappelzAuthEmu");
+	RappelzLibConfig::get()->app.configfile.setDefault("auth.opt");
+	RappelzLibConfig::get()->log.file.setDefault("auth.log");
+	RappelzLibConfig::get()->trafficDump.file.setDefault("auth_traffic.log");
 }
