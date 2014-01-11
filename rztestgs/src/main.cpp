3a3a8780e538e664f733dc49f8ae684d90b0639d
glandu2 <glandu2epvp@gmail.com>
2014-01-11 01:26:54 +0100

    Add global.version config value set to git rev and date

--- rztestgs/src/main.cpp
+++ rztestgs/src/main.cpp
@@ -2,6 +2,10 @@
 #include "uv.h"
 #include <string.h>
 #include "RappelzLibInit.h"
+#include "ConfigInfo.h"
+#include "RappelzPlayerCountGitVersion.h"
+
+static void init();
 
 int main(int argc, char *argv[])
 {
@@ -14,10 +18,15 @@ int main(int argc, char *argv[])
 	if(argc >= 3)
 		port = atoi(argv[2]);
 
-	RappelzLibInit(argc, argv, nullptr);
+	RappelzLibInit(argc, argv, &init);
+
 
 	PlayerCountMonitor playerCount(host, port);
 	playerCount.start();
 
 	uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+}
+
+static void init() {
+	CFG("global.version", RappelzPlayerCountVersion);
 }
