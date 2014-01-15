d984d5d4e4cd02f8223720709ce6c98333123f16
glandu2 <glandu2epvp@gmail.com>
2014-01-15 22:20:12 +0100

    host and port as config value

--- rztestgs/src/main.cpp
+++ rztestgs/src/main.cpp
@@ -9,19 +9,11 @@ static void init();
 
 int main(int argc, char *argv[])
 {
-	const char* host = "127.0.0.1";
-	uint16_t port = 4500;
-
-	if(argc >= 2)
-		host = argv[1];
-
-	if(argc >= 3)
-		port = atoi(argv[2]);
 
 	RappelzLibInit(argc, argv, &init);
 
 
-	PlayerCountMonitor playerCount(host, port);
+	PlayerCountMonitor playerCount(CFG("ip", "127.0.0.1").get(), CFG("port", 4500).get());
 	playerCount.start();
 
 	uv_run(uv_default_loop(), UV_RUN_DEFAULT);
@@ -29,4 +21,6 @@ int main(int argc, char *argv[])
 
 static void init() {
 	CFG("global.version", RappelzPlayerCountVersion);
+	CFG("ip", "127.0.0.1");
+	CFG("port", 4500);
 }
