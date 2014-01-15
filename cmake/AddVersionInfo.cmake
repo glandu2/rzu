137dd2e794015f03c44096803c0341391f52949c
glandu2 <glandu2epvp@gmail.com>
2014-01-15 22:27:03 +0100

    Fix Socket::close() bug when shutdown when ot connected. Use date from shell instead of cmake (command string(timestamp) is not supported in cmake < 2.8.9.

--- cmake/AddVersionInfo.cmake
+++ cmake/AddVersionInfo.cmake
@@ -8,14 +8,15 @@ set(GetGitVersion_LOCATION "${CMAKE_CURRENT_LIST_DIR}")
 function(add_git_version_info _targetname _sourcefiles)
 	include_directories(${CMAKE_BINARY_DIR})
 	add_custom_command(
-		OUTPUT "${CMAKE_BINARY_DIR}/${TARGET_NAME}GitVersion.cpp" "${CMAKE_BINARY_DIR}/${TARGET_NAME}GitVersion.h"
-		COMMAND ${CMAKE_COMMAND} -D SRC=${GetGitVersion_LOCATION}/GitVersion
-			-D DST=${CMAKE_BINARY_DIR}/${TARGET_NAME}GitVersion
+		OUTPUT "${CMAKE_BINARY_DIR}/${TARGET_NAME}GitVersion.cpp"
+		COMMAND ${CMAKE_COMMAND} -D SRC=${GetGitVersion_LOCATION}/GitVersion.cpp.in
+			-D DST=${CMAKE_BINARY_DIR}/${TARGET_NAME}GitVersion.cpp
 			-D TARGET_NAME=${TARGET_NAME}
 			-D WORKINGDIR=${CMAKE_CURRENT_LIST_DIR}
 			-P ${GetGitVersion_LOCATION}/GetGitVersion.cmake
-		DEPENDS ${${_sourcefiles}}
+		DEPENDS ${${_sourcefiles}} ${GetGitVersion_LOCATION}/GetGitVersion.cmake
 	)
+	configure_file(${GetGitVersion_LOCATION}/GitVersion.h.in ${CMAKE_BINARY_DIR}/${TARGET_NAME}GitVersion.h @ONLY)
 	set(${_sourcefiles} ${${_sourcefiles}} "${CMAKE_BINARY_DIR}/${TARGET_NAME}GitVersion.cpp" "${CMAKE_BINARY_DIR}/${TARGET_NAME}GitVersion.h" PARENT_SCOPE)
 endfunction()
 
--- cmake/GetGitVersion.cmake
+++ cmake/GetGitVersion.cmake
@@ -1,16 +1,34 @@
 find_package(Git QUIET)
 if(GIT_FOUND)
 execute_process(
-     COMMAND
-     ${GIT_EXECUTABLE} describe --always --dirty --abbrev=40
-     WORKING_DIRECTORY ${WORKINGDIR}
-     OUTPUT_VARIABLE GIT_VERSION
-     OUTPUT_STRIP_TRAILING_WHITESPACE
+	COMMAND ${GIT_EXECUTABLE} describe --always --dirty --abbrev=40
+	WORKING_DIRECTORY ${WORKINGDIR}
+	OUTPUT_VARIABLE GIT_VERSION
+	OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 else()
 set(GIT_VERSION "unknown-rev")
 endif()
 
-string(TIMESTAMP CURRENT_DATE UTC)
-configure_file(${SRC}.h.in ${DST}.h @ONLY)
-configure_file(${SRC}.cpp.in ${DST}.cpp @ONLY)
+if(WIN32)
+	execute_process(
+		COMMAND "cmd" "/C" "(for /f " tokens=1,2 delims== " %x in ('wmic path win32_utctime get year^,month^,day^,hour^,minute^,second /format:list ^| findstr ^=') do @set %x=0%y) && call echo %year:~-5,4%-%month:~-3,2%-%day:~-3,2%T%Hour:~-3,2%:%Minute:~-3,2%:%Second:~-3,2%Z"
+		OUTPUT_VARIABLE CURRENT_DATE
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+elseif(UNIX)
+	execute_process(
+		COMMAND "date" "-u" "+%Y-%m-%dT%H:%M:%SZ"
+		OUTPUT_VARIABLE CURRENT_DATE
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+else()
+	message(SEND_ERROR "date not implemented")
+	set(${CURRENT_DATE} 000000000000)
+endif()
+
+message("Build date: \"${CURRENT_DATE}\"")
+
+#string(TIMESTAMP CURRENT_DATE UTC)
+#set(CURRENT_DATE "11/01/2014 01:07")
+configure_file(${SRC} ${DST} @ONLY)
