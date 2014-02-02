7f5a3cff6847fa81c0c2d9dbd8614ff202a40a25
glandu2 <glandu2epvp@gmail.com>
2014-02-02 03:58:52 +0100

    Add missing BanManager class

---
+++ rzauth/src/BanManager.h
@@ -0,0 +1,24 @@
+#ifndef BANNEDIP_H
+#define BANNEDIP_H
+
+#include "Object.h"
+#include <unordered_set>
+#include <string>
+#include <stdint.h>
+
+class BanManager : public Object
+{
+public:
+	BanManager();
+
+	void loadFile();
+	bool isBanned(uint32_t ip);
+
+protected:
+	bool stringToIpNetwork(const char* ip, uint32_t* out);
+
+private:
+	std::unordered_set<uint32_t> bannedIps;
+};
+
+#endif // BANNEDIP_H
