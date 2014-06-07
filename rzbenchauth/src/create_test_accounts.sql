acaeb02e59aae5997949ff4253325fec379f96f9
glandu2 <glandu2epvp@gmail.com>
2014-06-07 22:40:43 +0200

    Timing functions (for run duration), sql file to create many accounts (pl/pgsql)

---
+++ rzbenchauth/src/create_test_accounts.sql
@@ -0,0 +1,14 @@
+﻿DO $$
+declare
+	accountid int;
+	accountname varchar(50);
+begin
+
+FOR i IN 13..10013 LOOP
+accountid := i;
+select 'test' || i -2 into accountname;
+insert into account values (accountid, accountname, '613b5247e3398350918cb622a3ec19e9');
+END LOOP;
+
+end;
+$$language plpgsql;
