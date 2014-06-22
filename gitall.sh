3b857b2f48fa1d8100e8a2aeed56c83576653edf
glandu2 <glandu2epvp@gmail.com>
2014-06-22 20:08:08 +0200

    Handle console close + ctrl+C. Fix lastServerIdx, handle user_ratio for server list

--- gitall.sh
+++ gitall.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
+echo RappelzUnified:
 git $*
+echo libuv:
 cd libuv
 git $*
+echo RappelzLib:
 cd ../RappelzLib
 git $*
+echo RappelzPlayerCount:
 cd ../RappelzPlayerCount
 git $*
+echo RappelzServerAuth:
 cd ../RappelzServerAuth
 git $*
+echo RappelzClient:
 cd ../RappelzClient
 git $*
+echo RappelzAuthBenchmark:
 cd ../RappelzAuthBenchmark
 git $*
 
