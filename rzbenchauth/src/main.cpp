16afcab762b6e0c4ca25df6da23ef2fdb30f0447
glandu2 <glandu2epvp@gmail.com>
2014-08-25 23:12:12 +0200

    Add connection only benchmark

--- rzbenchauth/src/main.cpp
+++ rzbenchauth/src/main.cpp
@@ -17,8 +17,13 @@ void onGameResult(IListener* instance, Authentication* auth, TS_ResultCode resul
 void onAuthClosed(IListener* instance, Authentication* auth);
 void onAuthClosedWithFailure(IListener* instance, Authentication* auth);
 
+void onSocketStateChange(IListener* instance, Socket* socket, Socket::State oldState, Socket::State newState);
+void onSocketTimer(uv_timer_t *handle);
+void onSocketClosed();
+
 std::vector<Account*> accounts;
 std::vector<Authentication*> auths;
+std::vector<Socket*> sockets; //for connections/sec test
 std::vector<uv_timer_t*> timers;
 bool printDebug = false;
 int connectionsStarted = 0;
@@ -26,6 +31,11 @@ int connectionsDone = 0;
 int connectionTargetCount;
 bool connectToGs = false;
 int delay = 0;
+bool benchConnections = false;
+
+std::string ip;
+int port;
+bool useLocalhost;
 
 static void init() {
 	CFG_CREATE("ip", "127.0.0.1" /*"127.0.0.1"*/);
@@ -40,6 +50,7 @@ static void init() {
 	CFG_CREATE("delay", 0);
 	CFG_CREATE("idxoffset", 0);
 	CFG_CREATE("usecperconnection", 0);
+	CFG_CREATE("benchconnection", false);
 	RappelzLibConfig::get()->app.appName.setDefault("RappelzBenchmarkAuth");
 	RappelzLibConfig::get()->app.configfile.setDefault("benchmarkauth.opt");
 	RappelzLibConfig::get()->log.file.setDefault("benchmarkauth.log");
@@ -56,14 +67,7 @@ static std::string getIpForConnection(const std::string& originalIp, bool useLoc
 	}
 }
 
-int main(int argc, char *argv[])
-{
-	RappelzLibInit();
-	init();
-	ConfigInfo::get()->init(argc, argv);
-	ConfigInfo::get()->dump();
-	initTimer();
-
+void benchmarkAuthentication() {
 	int count = CFG_GET("count")->getInt();
 	std::string accountNamePrefix = CFG_GET("account")->getString();
 	std::string ip = CFG_GET("ip")->getString();
@@ -71,12 +75,6 @@ int main(int argc, char *argv[])
 	bool usersa = CFG_GET("use_rsa")->getBool();
 	int idxoffset = CFG_GET("idxoffset")->getInt();
 	bool useLocalHost = ip == "127.0.0.1";
-	int usecBetweenConnection = CFG_GET("usecperconnection")->getInt();
-
-	connectionTargetCount = CFG_GET("targetcount")->getInt();
-	printDebug = CFG_GET("printall")->getBool();
-	connectToGs = CFG_GET("connecttogs")->getBool();
-	delay = CFG_GET("delay")->getInt();
 
 	accounts.reserve(count);
 	auths.reserve(count);
@@ -98,21 +96,91 @@ int main(int argc, char *argv[])
 			timer->data = auth;
 			timers.push_back(timer);
 		}
-
-
 	}
+}
 
-	fprintf(stderr, "Starting benchmark\n");
-
-	if(usecBetweenConnection == 0)
-		resetTimer();
-
+void startBenchAuth(int usecBetweenConnection) {
 	for(size_t i = 0; i < auths.size(); i++) {
 		auths[i]->connect(accounts[i], CFG_GET("password")->getString(), Callback<Authentication::CallbackOnAuthResult>(nullptr, &onAuthResult));
 		connectionsStarted++;
 		if(usecBetweenConnection)
 			tfMicroSleep(usecBetweenConnection);
 	}
+}
+
+void benchmarkConnections() {
+	int count = CFG_GET("count")->getInt();
+
+	sockets.reserve(count);
+	if(delay)
+		timers.reserve(count);
+	for(int i = 0; i < count; i++) {
+		Socket* socket = new Socket(EventLoop::getLoop(), false);
+		socket->addEventListener(nullptr, &onSocketStateChange);
+
+		sockets.push_back(socket);
+
+		if(delay) {
+			uv_timer_t* timer = new uv_timer_t;
+			uv_timer_init(EventLoop::getLoop(), timer);
+			timer->data = socket;
+			timers.push_back(timer);
+		}
+	}
+}
+
+void startBenchConnections(int usecBetweenConnection) {
+	ip = CFG_GET("ip")->getString();
+	port = CFG_GET("port")->getInt();
+	useLocalhost = ip == "127.0.0.1";
+
+	for(size_t i = 0; i < sockets.size(); i++) {
+		Socket* socket = sockets[i];
+		socket->connect(ip, port);
+		connectionsStarted++;
+		if(usecBetweenConnection)
+			tfMicroSleep(usecBetweenConnection);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	RappelzLibInit();
+	init();
+	ConfigInfo::get()->init(argc, argv);
+	ConfigInfo::get()->dump();
+	initTimer();
+
+	Log mainLogger(RappelzLibConfig::get()->log.enable,
+				   RappelzLibConfig::get()->log.level,
+				   RappelzLibConfig::get()->log.consoleLevel,
+				   RappelzLibConfig::get()->log.dir,
+				   RappelzLibConfig::get()->log.file,
+				   RappelzLibConfig::get()->log.maxQueueSize);
+	Log::setDefaultLogger(&mainLogger);
+
+	int usecBetweenConnection = CFG_GET("usecperconnection")->getInt();
+
+	connectionTargetCount = CFG_GET("targetcount")->getInt();
+	printDebug = CFG_GET("printall")->getBool();
+	connectToGs = CFG_GET("connecttogs")->getBool();
+	delay = CFG_GET("delay")->getInt();
+	benchConnections = CFG_GET("benchconnection")->getBool();
+
+	if(benchConnections)
+		benchmarkConnections();
+	else
+		benchmarkAuthentication();
+
+	fprintf(stderr, "Starting benchmark\n");
+
+	if(usecBetweenConnection == 0)
+		resetTimer();
+
+	if(benchConnections)
+		startBenchConnections(usecBetweenConnection);
+	else
+		startBenchAuth(usecBetweenConnection);
 
 	if(usecBetweenConnection != 0) {
 		fprintf(stderr, "Connected %d connections at limited speed, continuing benchmark at full-speed (time counter begin now)\n", connectionsStarted);
@@ -126,6 +194,7 @@ int main(int argc, char *argv[])
 	fprintf(stderr, "%d connections in %llu usec => %f auth/sec\n", connectionsDone, duration, connectionsDone/((float)duration/1000000.0f));
 }
 
+//auth bench
 void onAuthResult(IListener* instance, Authentication* auth, TS_ResultCode result, const std::string& resultString) {
 	if(printDebug || result != TS_RESULT_SUCCESS)
 		fprintf(stderr, "%s: Auth result: %d (%s)\n", auth->getAccountName().c_str(), result, resultString.empty() ? "no associated string" : resultString.c_str());
@@ -190,4 +259,26 @@ void onGameResult(IListener* instance, Authentication* auth, TS_ResultCode resul
 			auth->connect(nullptr, CFG_GET("password")->getString(), Callback<Authentication::CallbackOnAuthResult>(nullptr, &onAuthResult));
 		}
 	}
+}
+
+//conn bench
+void onSocketStateChange(IListener* instance, Socket* socket, Socket::State oldState, Socket::State newState) {
+	if(newState == Socket::ConnectedState) {
+		socket->close();
+//		if(delay == 0)
+//			socket->close();
+//		else
+//			uv_timer_start(timers[reinterpret_cast<intptr_t>(instance)], &onSocketTimer, delay, 0);
+	} else if(newState == Socket::UnconnectedState) {
+		connectionsDone++;
+		if(connectionsStarted < connectionTargetCount) {
+			connectionsStarted++;
+			socket->connect(ip, port);
+		}
+	}
+}
+
+void onSocketTimer(uv_timer_t* handle) {
+	Socket* socket = (Socket*)handle->data;
+	socket->close();
 }
