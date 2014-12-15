118b29a2347d0c33b86bf39fdc086fe7e6437d4d
glandu2 <glandu2epvp@gmail.com>
2014-12-16 00:54:49 +0100

    Use PacketSession instead of RappelzSocket. Use non static functions

--- rztestgs/src/PlayerCountMonitor.cpp
+++ rztestgs/src/PlayerCountMonitor.cpp
@@ -6,7 +6,7 @@
 #include <string.h>
 #include "EventLoop.h"
 
-PlayerCountMonitor::PlayerCountMonitor(std::string host, uint16_t port, const std::string &reqStr, int intervalms) : sock(EventLoop::getLoop(), EncryptedSocket::Encrypted) {
+PlayerCountMonitor::PlayerCountMonitor(std::string host, uint16_t port, const std::string &reqStr, int intervalms) {
 	this->host = host;
 	this->port = port;
 	this->connectedTimes = 0;
@@ -14,48 +14,42 @@ PlayerCountMonitor::PlayerCountMonitor(std::string host, uint16_t port, const st
 
 	printf("#msec since epoch, players connected number, process load\n");
 
-	sock.addPacketListener(TS_CA_VERSION::packetID, this, onPlayerCountReceived);
-	sock.addPacketListener(TS_CC_EVENT::packetID, this, onPlayerCountReceived);
-
 	uv_timer_init(EventLoop::getLoop(), &timer);
-	timer.data = this;
 	timeout = intervalms;
 }
 
 void PlayerCountMonitor::start() {
-	 uv_timer_start(&timer, &updatePlayerNumber, 0, timeout);
+	uv_timer_start(&timer, &updatePlayerNumberStatic, 0, timeout);
+	timer.data = this;
 }
 
 void PlayerCountMonitor::stop() {
-	 uv_timer_stop(&timer);
+	uv_timer_stop(&timer);
 }
 
-void PlayerCountMonitor::updatePlayerNumber(uv_timer_t* handle) {
-	PlayerCountMonitor* thisInstance = (PlayerCountMonitor*)handle->data;
-
-	if(thisInstance->sock.getState() == Stream::UnconnectedState) {
-		thisInstance->connectedTimes = 0;
-		thisInstance->sock.connect(thisInstance->host, thisInstance->port);
-	} else if(thisInstance->sock.getState() == Stream::ConnectingState) {
-		thisInstance->sock.close();
-		thisInstance->error("Server connection timeout\n");
+void PlayerCountMonitor::updatePlayerNumberStatic(uv_timer_t* handle) { static_cast<PlayerCountMonitor*>(handle->data)->updatePlayerNumber(); }
+void PlayerCountMonitor::updatePlayerNumber() {
+	if(getStream() == nullptr || getStream()->getState() == Stream::UnconnectedState) {
+		connectedTimes = 0;
+		connect(host.c_str(), port);
+	} else if(getStream()->getState() == Stream::ConnectingState) {
+		getStream()->close();
+		error("Server connection timeout\n");
 	} else {
-		if(thisInstance->sock.getState() == Stream::ConnectedState) {
-			thisInstance->connectedTimes++;
+		if(getStream()->getState() == Stream::ConnectedState) {
+			connectedTimes++;
 		}
 
 		//we see more than 5 times the socket connected, it should disconnect, close it in case of hanged state
-		if(thisInstance->connectedTimes >= 5) {
-			thisInstance->sock.abort();
-			thisInstance->connectedTimes = 0;
+		if(connectedTimes >= 5) {
+			abortSession();
+			connectedTimes = 0;
 		}
-		thisInstance->error("Timer tick but server is not unconnected, timer is too fast ?\n");
+		error("Timer tick but server is not unconnected, timer is too fast ?\n");
 	}
 }
 
-void PlayerCountMonitor::onPlayerCountReceived(IListener* instance, RappelzSocket* sock, const TS_MESSAGE* packetData) {
-	PlayerCountMonitor* thisInstance = (PlayerCountMonitor*)instance;
-
+void PlayerCountMonitor::onPacketReceived(const TS_MESSAGE* packetData) {
 	switch(packetData->id) {
 		case TS_SC_RESULT::packetID:
 		{
@@ -64,7 +58,7 @@ void PlayerCountMonitor::onPlayerCountReceived(IListener* instance, RappelzSocke
 				case TS_CA_VERSION::packetID:
                     printf("%lu %d %d\n", (unsigned long)time(NULL), result->value ^ 0xADADADAD, result->result);
 					fflush(stdout);
-					sock->close();
+					close();
 					break;
 			}
 			break;
@@ -79,15 +73,15 @@ void PlayerCountMonitor::onPlayerCountReceived(IListener* instance, RappelzSocke
 #ifndef NDEBUG
 				memset(versionMsg.szVersion, 0, sizeof(versionMsg.szVersion));
 #endif
-				strcpy(versionMsg.szVersion, thisInstance->reqStr.c_str());
+				strcpy(versionMsg.szVersion, reqStr.c_str());
 
-				sock->sendPacket(&versionMsg);
+				sendPacket(&versionMsg);
 			}
 			if(eventMsg->event == TS_CC_EVENT::CE_ServerConnectionLost) {
-				thisInstance->error("Disconnected from server !\n");
+				error("Disconnected from server !\n");
 			}
 			if(eventMsg->event == TS_CC_EVENT::CE_ServerUnreachable) {
-				thisInstance->error("Unreachable server !\n");
+				error("Unreachable server !\n");
 			}
 			break;
 		}
