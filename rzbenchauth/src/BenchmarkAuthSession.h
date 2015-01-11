e978b870202f6245b150f485c35daa706d7bd682
glandu2 <glandu2epvp@gmail.com>
2015-01-11 02:05:00 +0100

    Refactor to use ClientAuthSession class

---
+++ rzbenchauth/src/BenchmarkAuthSession.h
@@ -0,0 +1,47 @@
+#ifndef AUTHSESSION_H
+#define AUTHSESSION_H
+
+#include "ClientAuthSession.h"
+#include <string>
+
+struct BenchmarkConfig {
+	uint16_t port;
+	ClientAuthSession::AuthCipherMethod method;
+	std::string version;
+	int delay;
+	int connectionsDone;
+	int connectionsStarted;
+	int connectionTargetCount;
+};
+
+class BenchmarkAuthSession : public ClientAuthSession
+{
+public:
+	BenchmarkAuthSession(BenchmarkConfig* config);
+
+	void connect(const std::string& ip, const std::string& account, const std::string& password);
+
+private:
+	using ClientAuthSession::connect;
+
+	virtual void onAuthDisconnected();
+	virtual void onAuthResult(TS_ResultCode result, const std::string& resultString);
+	virtual void onServerList(const std::vector<ServerInfo>& servers, uint16_t lastSelectedServerId);
+
+	virtual void onGameDisconnected();
+	virtual void onGameResult(TS_ResultCode result);
+
+	static void onAuthDelayExpired(uv_timer_t* timer);
+
+private:
+	BenchmarkConfig* config;
+	std::string ip;
+	std::string account;
+	std::string password;
+
+	bool doReconnect;
+	uv_timer_t delayTimer;
+
+};
+
+#endif // AUTHSESSION_H
--- rzbenchauth/src/main.cpp
+++ rzbenchauth/src/main.cpp
@@ -1,35 +1,20 @@
 #include "uv.h"
-#include "Authentication.h"
+#include "BenchmarkAuthSession.h"
 #include <string.h>
 #include "EventLoop.h"
 #include "RappelzLibInit.h"
 #include <stdio.h>
-#include "PacketSession.h"
 #include "ConfigInfo.h"
 #include "RappelzLibConfig.h"
 #include "TimingFunctions.h"
 
-void onAuthResult(IListener* instance, Authentication* auth, TS_ResultCode result, const std::string &resultString);
-void onAuthRetrieveServer(uv_timer_t* handle);
-void onServerList(IListener* instance, Authentication* auth, const std::vector<Authentication::ServerInfo>* servers, uint16_t lastSelectedServerId);
-void onGameResult(IListener* instance, Authentication* auth, TS_ResultCode result, PacketSession *gameServerSocket);
-void onAuthClosed(IListener* instance, Authentication* auth);
-void onAuthClosedWithFailure(IListener* instance, Authentication* auth);
-
 void onSocketStateChange(IListener* instance, Stream *socket, Stream::State oldState, Stream::State newState);
-void onSocketTimer(uv_timer_t *handle);
-void onSocketClosed();
 
-std::vector<std::string> accounts;
-std::vector<Authentication*> auths;
+BenchmarkConfig config;
+std::vector<BenchmarkAuthSession*> auths;
+
 std::vector<Stream*> sockets; //for connections/sec test
-std::vector<uv_timer_t*> timers;
-bool printDebug = false;
-int connectionsStarted = 0;
-int connectionsDone = 0;
-int connectionTargetCount;
-bool connectToGs = false;
-int delay = 0;
+
 bool benchConnections = false;
 
 std::string ip;
@@ -44,8 +29,6 @@ static void init() {
 	CFG_CREATE("targetcount", 3000);
 	CFG_CREATE("password", "admin");
 	CFG_CREATE("use_rsa", false);
-	CFG_CREATE("printall", false);
-	CFG_CREATE("connecttogs", false);
 	CFG_CREATE("delay", 0);
 	CFG_CREATE("idxoffset", 0);
 	CFG_CREATE("usecperconnection", 0);
@@ -67,42 +50,32 @@ static std::string getIpForConnection(const std::string& originalIp, bool useLoc
 		return originalIp;
 	}
 }
-
 void benchmarkAuthentication() {
 	int count = CFG_GET("count")->getInt();
-	std::string accountNamePrefix = CFG_GET("account")->getString();
-	std::string ip = CFG_GET("ip")->getString();
-	int port = CFG_GET("port")->getInt();
-	bool usersa = CFG_GET("use_rsa")->getBool();
-	int idxoffset = CFG_GET("idxoffset")->getInt();
-	bool useLocalHost = ip == "127.0.0.1";
 
-	accounts.reserve(count);
+	config.port = CFG_GET("port")->getInt();
+	config.connectionTargetCount = CFG_GET("targetcount")->getInt();
+	config.delay = CFG_GET("delay")->getInt();
+	config.method = CFG_GET("use_rsa")->getBool()? ClientAuthSession::ACM_RSA_AES : ClientAuthSession::ACM_DES;
+	config.version = "200701120";
+
 	auths.reserve(count);
-	if(delay)
-		timers.reserve(count);
 	for(int i = 0; i < count; i++) {
-		const std::string account = (count > 1)? accountNamePrefix + std::to_string((long long)i + idxoffset) : accountNamePrefix;
-
-		Authentication* auth = new Authentication(getIpForConnection(ip, useLocalHost, i), usersa? Authentication::ACM_RSA_AES : Authentication::ACM_DES, port);
-		auth->index = i;
-
-		accounts.push_back(account);
+		BenchmarkAuthSession* auth = new BenchmarkAuthSession(&config);
 		auths.push_back(auth);
-
-		if(delay) {
-			uv_timer_t* timer = new uv_timer_t;
-			uv_timer_init(EventLoop::getLoop(), timer);
-			timer->data = auth;
-			timers.push_back(timer);
-		}
 	}
 }
 
 void startBenchAuth(int usecBetweenConnection) {
+	std::string accountNamePrefix = CFG_GET("account")->getString();
+	std::string ip = CFG_GET("ip")->getString();
+	int idxoffset = CFG_GET("idxoffset")->getInt();
+	bool useLocalHost = ip == "127.0.0.1";
+
 	for(size_t i = 0; i < auths.size(); i++) {
-		auths[i]->connect(accounts[i], CFG_GET("password")->getString(), Callback<Authentication::CallbackOnAuthResult>(nullptr, &onAuthResult));
-		connectionsStarted++;
+		const std::string account = (auths.size() > 1)? accountNamePrefix + std::to_string((long long)i + idxoffset) : accountNamePrefix;
+
+		auths[i]->connect(getIpForConnection(ip, useLocalHost, i), account, CFG_GET("password")->getString());
 		if(usecBetweenConnection)
 			tfMicroSleep(usecBetweenConnection);
 	}
@@ -112,20 +85,11 @@ void benchmarkConnections() {
 	int count = CFG_GET("count")->getInt();
 
 	sockets.reserve(count);
-	if(delay)
-		timers.reserve(count);
 	for(int i = 0; i < count; i++) {
 		Stream* socket = new Socket(EventLoop::getLoop(), false);
 		socket->addEventListener(nullptr, &onSocketStateChange);
 
 		sockets.push_back(socket);
-
-		if(delay) {
-			uv_timer_t* timer = new uv_timer_t;
-			uv_timer_init(EventLoop::getLoop(), timer);
-			timer->data = socket;
-			timers.push_back(timer);
-		}
 	}
 }
 
@@ -137,7 +101,7 @@ void startBenchConnections(int usecBetweenConnection) {
 	for(size_t i = 0; i < sockets.size(); i++) {
 		Stream* socket = sockets[i];
 		socket->connect(ip, port);
-		connectionsStarted++;
+		config.connectionsStarted++;
 		if(usecBetweenConnection)
 			tfMicroSleep(usecBetweenConnection);
 	}
@@ -161,18 +125,17 @@ int main(int argc, char *argv[])
 
 	int usecBetweenConnection = CFG_GET("usecperconnection")->getInt();
 
-	connectionTargetCount = CFG_GET("targetcount")->getInt();
-	printDebug = CFG_GET("printall")->getBool();
-	connectToGs = CFG_GET("connecttogs")->getBool();
-	delay = CFG_GET("delay")->getInt();
 	benchConnections = CFG_GET("benchconnection")->getBool();
+
+	config.connectionsDone = config.connectionsStarted = 0;
+	config.connectionTargetCount = CFG_GET("targetcount")->getInt();
 
 	if(benchConnections)
 		benchmarkConnections();
 	else
 		benchmarkAuthentication();
 
-	fprintf(stderr, "Starting benchmark\n");
+	mainLogger.log(Log::LL_Info, "main", 4, "Starting benchmark\n");
 
 	if(usecBetweenConnection == 0)
 		resetTimer();
@@ -183,7 +146,7 @@ int main(int argc, char *argv[])
 		startBenchAuth(usecBetweenConnection);
 
 	if(usecBetweenConnection != 0) {
-		fprintf(stderr, "Connected %d connections at limited speed, continuing benchmark at full-speed (time counter begin now)\n", connectionsStarted);
+		mainLogger.log(Log::LL_Info, "main", 4, "Connected %d connections at limited speed, continuing benchmark at full-speed (time counter begin now)\n", config.connectionsStarted);
 		resetTimer();
 	}
 
@@ -191,94 +154,18 @@ int main(int argc, char *argv[])
 
 	unsigned long long int duration = getTimerValue();
 
-	fprintf(stderr, "%d connections in %llu usec => %f auth/sec\n", connectionsDone, duration, connectionsDone/((float)duration/1000000.0f));
-}
-
-//auth bench
-void onAuthResult(IListener* instance, Authentication* auth, TS_ResultCode result, const std::string& resultString) {
-	if(printDebug || result != TS_RESULT_SUCCESS)
-		fprintf(stderr, "%s: Auth result: %d (%s)\n", auth->getAccountName().c_str(), result, resultString.empty() ? "no associated string" : resultString.c_str());
-	if(result == TS_RESULT_SUCCESS) {
-		if(delay == 0)
-			auth->retreiveServerList(Callback<Authentication::CallbackOnServerList>(nullptr, &onServerList));
-		else {
-			uv_timer_start(timers[auth->index], &onAuthRetrieveServer, delay, 0);
-		}
-	} else {
-		auth->abort(Callback<Authentication::CallbackOnAuthClosed>(nullptr, &onAuthClosedWithFailure));
-	}
-}
-
-void onAuthRetrieveServer(uv_timer_t* handle) {
-	Authentication* auth = (Authentication*) handle->data;
-
-	auth->retreiveServerList(Callback<Authentication::CallbackOnServerList>(nullptr, &onServerList));
-}
-
-void onServerList(IListener* instance, Authentication* auth, const std::vector<Authentication::ServerInfo>* servers, uint16_t lastSelectedServerId) {
-	if(printDebug) {
-		fprintf(stderr, "%p Server list (last id: %d)\n", auth, lastSelectedServerId);
-		for(size_t i = 0; i < servers->size(); i++) {
-			fprintf(stderr, "%d: %20s at %16s:%d %d%% user ratio\n",
-					servers->at(i).serverId,
-					servers->at(i).serverName.c_str(),
-					servers->at(i).serverIp.c_str(),
-					servers->at(i).serverPort,
-					servers->at(i).userRatio);
-		}
-	}
-	if(connectToGs && servers->size() > 0)
-		auth->selectServer(servers->at(0).serverId, Callback<Authentication::CallbackOnGameResult>(nullptr, &onGameResult));
-	else
-		auth->abort(Callback<Authentication::CallbackOnAuthClosed>(nullptr, &onAuthClosed));
-}
-
-void onAuthClosedWithFailure(IListener* instance, Authentication* auth) {
-	if(printDebug)
-		fprintf(stderr, "%p auth closed with failure\n", auth);
-	//auth->connect(nullptr, CFG_GET("password")->getString(), Callback<Authentication::CallbackOnAuthResult>(nullptr, &onAuthResult));
-}
-
-void onAuthClosed(IListener* instance, Authentication* auth) {
-	if(printDebug)
-		fprintf(stderr, "%p auth closed\n", auth);
-	connectionsDone++;
-	if(connectionsStarted < connectionTargetCount) {
-		connectionsStarted++;
-		auth->connect(nullptr, CFG_GET("password")->getString(), Callback<Authentication::CallbackOnAuthResult>(nullptr, &onAuthResult));
-	}
-}
-
-void onGameResult(IListener* instance, Authentication* auth, TS_ResultCode result, PacketSession * gameServerSocket) {
-	fprintf(stderr, "login to GS result: %d\n", result);
-	if(gameServerSocket) {
-		connectionsDone++;
-		gameServerSocket->getStream()->close();
-		if(connectionsStarted < connectionTargetCount) {
-			connectionsStarted++;
-			auth->connect(nullptr, CFG_GET("password")->getString(), Callback<Authentication::CallbackOnAuthResult>(nullptr, &onAuthResult));
-		}
-	}
+	mainLogger.log(Log::LL_Info, "main", 4, "%d connections in %llu usec => %f auth/sec\n", config.connectionsDone, duration, config.connectionsDone/((float)duration/1000000.0f));
 }
 
 //conn bench
 void onSocketStateChange(IListener* instance, Stream* socket, Stream::State oldState, Stream::State newState) {
 	if(newState == Stream::ConnectedState) {
 		socket->close();
-//		if(delay == 0)
-//			socket->close();
-//		else
-//			uv_timer_start(timers[reinterpret_cast<intptr_t>(instance)], &onSocketTimer, delay, 0);
 	} else if(newState == Stream::UnconnectedState) {
-		connectionsDone++;
-		if(connectionsStarted < connectionTargetCount) {
-			connectionsStarted++;
+		config.connectionsDone++;
+		if(config.connectionsStarted < config.connectionTargetCount) {
+			config.connectionsStarted++;
 			socket->connect(ip, port);
 		}
 	}
-}
-
-void onSocketTimer(uv_timer_t* handle) {
-	Stream* socket = (Stream*)handle->data;
-	socket->close();
 }
