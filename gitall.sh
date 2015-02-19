fc2a908e07fc99f200dcdce3bcd031bb703354ad
glandu2 <glandu2epvp@gmail.com>
2015-02-19 19:57:42 +0100

    Add auth logs to LogServer and GS auto reconnection support for auth + proxy tool

--- gitall.sh
+++ gitall.sh
@@ -6,6 +6,7 @@ echo libuv: && cd libuv && git $*
 echo librzu: && cd ../librzu && git $*
 echo rzauth: && cd ../rzauth && git $*
 echo rzbenchauth: && cd ../rzbenchauth && git $*
+echo rzgamereconnect: && cd ../rzgamereconnect && git $*
 echo rzchatgateway: && cd ../rzchatgateway && git $*
 echo rzplayercount: && cd ../rzplayercount && git $*
 echo VStructGen: && cd ../VStructGen && git $*
