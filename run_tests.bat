5371b0bd36880c50f24d0f0a3c3f11e7b9bcb033
glandu2 <glandu2epvp@gmail.com>
2015-05-02 20:16:46 +0200

    Add IMBC tests. Optimize logging to file

---
+++ run_tests.bat
@@ -0,0 +1,17 @@
+start rzauth /configfile:auth-test.opt
+rzauth_test --gtest_repeat=2
+IF ERRORLEVEL 1 (
+	echo terminate | %~dp0\nc 127.0.0.1 4501
+	GOTO end
+)
+start rzgamereconnect
+rzauth_test /game.port:4802 --gtest_repeat=2 --gtest_filter=-TS_GA_LOGIN_WITH_LOGOUT.*
+IF ERRORLEVEL 1 (
+	echo terminate | %~dp0\nc 127.0.0.1 4501
+	echo terminate | %~dp0\nc 127.0.0.1 4801
+	GOTO end
+)
+echo terminate | %~dp0\nc 127.0.0.1 4501
+rzgamereconnect_test --gtest_repeat=2
+echo terminate | %~dp0\nc 127.0.0.1 4801
+:end
---
+++ run_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+nc_location=$(which nc)
+if [ -z "$nc_location" ]; then
+	nc_location="../nc.exe"
+fi
+rzauth /configfile:auth-test.opt 2> /dev/null &
+rzauth_test --gtest_repeat=2
+if [ $? -ne 0 ]; then
+	echo terminate | $nc_location 127.0.0.1 4501
+	exit
+fi
+rzgamereconnect 2> /dev/null &
+rzauth_test /game.port:4802 --gtest_repeat=2 --gtest_filter=-TS_GA_LOGIN_WITH_LOGOUT.*
+if [ $? -ne 0 ]; then
+	echo terminate | $nc_location 127.0.0.1 4501
+	echo terminate | $nc_location 127.0.0.1 4801
+	exit
+fi
+echo terminate | $nc_location 127.0.0.1 4501
+rzgamereconnect_test --gtest_repeat=2
+echo terminate | $nc_location 127.0.0.1 4801
