7be7cf65ede92099b77f7f9d71aad30fee92f941
glandu2 <glandu2epvp@gmail.com>
2015-08-18 21:32:50 +0200

    Add rzgame to gitall.sh

--- gitall.sh
+++ gitall.sh
@@ -9,6 +9,7 @@ echo ----rztest: && cd ../rztest && git $*
 echo ----zlib: && cd ../zlib && git $*
 echo ----rzauth: && cd ../rzauth && git $*
 echo ----rzbenchauth: && cd ../rzbenchauth && git $*
+echo ----rzgame: && cd ../rzgame && git $*
 echo ----rzgamereconnect: && cd ../rzgamereconnect && git $*
 echo ----rzchatgateway: && cd ../rzchatgateway && git $*
 echo ----rzplayercount: && cd ../rzplayercount && git $*
