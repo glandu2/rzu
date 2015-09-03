2f9139d19b7296ed60e36efe877e5b1e3962c1e2
glandu2 <glandu2epvp@gmail.com>
2015-09-04 00:08:44 +0200

    Replace AdminInterface with ConsoleSession

--- rzgamereconnect/src/ClassCounter.cpp
+++ rzgamereconnect/src/ClassCounter.cpp
@@ -1,18 +1,5 @@
 #include "ClassCounter.h"
 
-//generated with notepad++ with this pattern: (.*) ->
-//#include "\1.h"\nDECLARE_CLASSCOUNT_STATIC\(\1\)\n
-/* Class list:
-ClientData
-ClientInfo
-DB_Account
-DesPasswordCipher
-ServerInfo
-*/
-
-#include "AdminServer/AdminInterface.h"
-DECLARE_CLASSCOUNT_STATIC(AdminServer::AdminInterface)
-
 #include "AuthServer/AuthSession.h"
 DECLARE_CLASSCOUNT_STATIC(AuthServer::AuthSession)
 
