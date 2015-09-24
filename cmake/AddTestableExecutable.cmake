222a211ea784c1767cf2db1f590c8e6e411bd4a1
glandu2 <glandu2epvp@gmail.com>
2015-09-25 00:20:30 +0200

    Rework db column mapping and add .exe icons made by Gr4ph0s

--- cmake/AddTestableExecutable.cmake
+++ cmake/AddTestableExecutable.cmake
@@ -1,14 +1,30 @@
 function(add_exe name sources)
   if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/metadata.rc.in")
-    file(GLOB ICON_FILES "../res/*.ico")
+    file(GLOB RES_FILES "../res/*")
+
+    # Construct FILEVERSION (4 numbers separated by comma)
+    string(REPLACE "." ";" VERSION_LIST ${CPACK_PACKAGE_VERSION})
+    list(LENGTH VERSION_LIST VERSION_LIST_SIZE)
+    foreach(IDX RANGE 0 3)
+        if(${IDX} LESS ${VERSION_LIST_SIZE})
+            list(GET VERSION_LIST ${IDX} VERSION_ELT)
+            list(APPEND METADATA_VERSION_LIST ${VERSION_ELT})
+        else()
+            list(APPEND METADATA_VERSION_LIST "0")
+        endif()
+    endforeach()
+    string(REPLACE ";" "," METADATA_VERSION "${METADATA_VERSION_LIST}")
+
     add_custom_command(
         OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/metadata.rc"
         COMMAND ${CMAKE_COMMAND} -D SRC="${CMAKE_CURRENT_SOURCE_DIR}/metadata.rc.in"
             -D DST="${CMAKE_CURRENT_BINARY_DIR}/metadata.rc"
             -D TARGET_NAME=${TARGET_NAME}
             -D WORKINGDIR=${CMAKE_CURRENT_SOURCE_DIR}
+            -D TARGET_FILEVERSION=${METADATA_VERSION}
+            -D TARGET_VERSION=${CPACK_PACKAGE_VERSION}
             -P ${CUSTOM_CMAKE_MODULES_PATH}/GetGitVersion.cmake
-        DEPENDS ${ICON_FILES} "${CMAKE_CURRENT_SOURCE_DIR}/metadata.rc.in" ${CUSTOM_CMAKE_MODULES_PATH}/GetGitVersion.cmake
+        DEPENDS ${RES_FILES} "${CMAKE_CURRENT_SOURCE_DIR}/metadata.rc.in" ${CUSTOM_CMAKE_MODULES_PATH}/GetGitVersion.cmake
     )
     list(APPEND sources "${CMAKE_CURRENT_BINARY_DIR}/metadata.rc" "${CMAKE_CURRENT_SOURCE_DIR}/metadata.rc.in")
   endif()
--- run_tests.sh
+++ run_tests.sh
@@ -4,19 +4,21 @@ nc_location=$(which nc)
 if [ -z "$nc_location" ]; then
 	nc_location="../nc.exe"
 fi
-rzauth /configfile:auth-test.opt 2> /dev/null &
-rzauth_test --gtest_repeat=2
+./rzauth /configfile:auth-test.opt 2> /dev/null &
+sleep 1
+./rzauth_test --gtest_repeat=2
 if [ $? -ne 0 ]; then
 	echo terminate | $nc_location 127.0.0.1 4501
 	exit
 fi
-rzgamereconnect 2> /dev/null &
-rzauth_test /game.port:4802 --gtest_repeat=2 --gtest_filter=-TS_GA_LOGIN_WITH_LOGOUT.*
+./rzgamereconnect 2> /dev/null &
+sleep 1
+./rzauth_test /game.port:4802 --gtest_repeat=2 --gtest_filter=-TS_GA_LOGIN_WITH_LOGOUT.*
 if [ $? -ne 0 ]; then
 	echo terminate | $nc_location 127.0.0.1 4501
 	echo terminate | $nc_location 127.0.0.1 4801
 	exit
 fi
 echo terminate | $nc_location 127.0.0.1 4501
-rzgamereconnect_test --gtest_repeat=2
+./rzgamereconnect_test --gtest_repeat=2
 echo terminate | $nc_location 127.0.0.1 4801
