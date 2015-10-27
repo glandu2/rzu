493e8eaaf4dfe81b1b10279ce8feed8c196171bf
glandu2 <glandu2epvp@gmail.com>
2015-10-27 15:45:41 +0100

    gs: partial login

--- generateGuards.sh
+++ generateGuards.sh
@@ -4,7 +4,5 @@ cd librzu/src && find . -name '*.h' | python ../../generateGuards.py
 cd ../../rzauth/src && find . -name '*.h' | python ../../generateGuards.py
 cd ../../rzbenchauth/src && find . -name '*.h' | python ../../generateGuards.py
 cd ../../rzgame/src && find . -name '*.h' | python ../../generateGuards.py
-cd ../../PlayerCount/src && find . -name '*.h' | python ../../generateGuards.py
-cd ../../ChatGateway/src && find . -name '*.h' | python ../../generateGuards.py
-cd ../../VStructGen/src && find . -name '*.h' | python ../../generateGuards.py
-
+cd ../../rzplayercount/src && find . -name '*.h' | python ../../generateGuards.py
+cd ../../rzchatgateway/src && find . -name '*.h' | python ../../generateGuards.py
