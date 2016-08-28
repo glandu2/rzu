dbfe9ce985b2264106131c47497f1f49c9587ac9
glandu2 <glandu2epvp@gmail.com>
2016-08-28 04:30:50 +0200

    Support openssl 1.1

--- run_tests.sh
+++ run_tests.sh
@@ -11,7 +11,7 @@ if [ $? -ne 0 ]; then
 	echo terminate | $nc_location 127.0.0.1 4501
 	exit
 fi
-./rzgamereconnect 2> /dev/null &
+./rzgamereconnect /auth.reconnectdelay:100 2> /dev/null &
 sleep 1
 ./rzauth_test /game.port:4802 --gtest_repeat=2 --gtest_filter=-TS_GA_LOGIN_WITH_LOGOUT.*
 if [ $? -ne 0 ]; then
