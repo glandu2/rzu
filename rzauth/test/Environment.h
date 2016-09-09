553bab0b6f4e09d94b04c1e0be90f5709bb580c4
glandu2 <glandu2epvp@gmail.com>
2016-09-10 01:31:35 +0200

    Update with tests rearrangement in rztest. Add execution of rzauth and rzgamereconnect

---
+++ rzauth/test/Environment.h
@@ -0,0 +1,22 @@
+#ifndef ENVIRONMENT_H
+#define ENVIRONMENT_H
+
+#include "TestEnvironment.h"
+
+class Environment : public TestEnvironment
+{
+public:
+	Environment() : testGameReconnect(false) { instance = this; }
+	virtual ~Environment() {}
+	virtual void beforeTests();
+	virtual void afterTests();
+
+	static bool isGameReconnectBeingTested();
+
+	bool testGameReconnect;
+
+private:
+	static Environment* instance;
+};
+
+#endif
