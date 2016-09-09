7e36c0926ddda106c3646604a880d6ec38c94c23
glandu2 <glandu2epvp@gmail.com>
2016-09-10 01:35:08 +0200

    Use Timer<> instead of uv_timer_t to correctly manage timer memory (stop is not sufficient to allow delete of timer)

--- rzclientreconnect/test/main.cpp
+++ rzclientreconnect/test/main.cpp
@@ -1,10 +1,13 @@
 #include "RunTests.h"
 #include "AuthServer/GlobalConfig.h"
+#include "Environment.h"
 
 static void initConfigs() {
 	AuthServer::GlobalConfig::init();
 }
 
 int main(int argc, char **argv) {
-	return runTests(argc, argv, &initConfigs);
+	TestRunner testRunner(argc, argv, &initConfigs);
+	::testing::AddGlobalTestEnvironment(new Environment);
+	return testRunner.runTests();
 }
