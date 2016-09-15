1ee40917b81af12d81168fd50c66d11355115390
glandu2 <glandu2epvp@gmail.com>
2016-09-15 02:04:17 +0200

    Use DRIVER as unixodbc was case sensitive and make it configurable in cmake

--- rzauth/test/AuthServer/GlobalConfig.cpp
+++ rzauth/test/AuthServer/GlobalConfig.cpp
@@ -12,9 +12,9 @@ void GlobalConfig::init() {
 	GlobalConfig::get();
 
 #ifdef _WIN32
-	GlobalConfig::get()->connectionString.setDefault("Driver={SQLite3 ODBC Driver};Database=AuthDatabase.db;");
+	GlobalConfig::get()->connectionString.setDefault("DRIVER={SQLite3 ODBC Driver};Database=AuthDatabase.db;");
 #else
-	GlobalConfig::get()->connectionString.setDefault("Driver={SQLite3};Database=AuthDatabase.db;");
+	GlobalConfig::get()->connectionString.setDefault("DRIVER=SQLite3;Database=AuthDatabase.db;");
 #endif
 
 }
