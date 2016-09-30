b1dc153c97092b23f0cf382f864aca16af68ea27
glandu2 <glandu2epvp@gmail.com>
2016-09-30 02:09:04 +0200

    Use true report

--- rzauctionmonitor/src/GameSession.cpp
+++ rzauctionmonitor/src/GameSession.cpp
@@ -45,7 +45,7 @@ void GameSession::onGameConnected() {
 	epochTimeOffset = rappelzTimeOffset = 0;
 
 	TS_CS_REPORT reportPkt;
-	reportPkt.report = "";
+	reportPkt.report = "\x5A\x76\x3F\xED\xD9\xEDWindows  (6.2.9200)|Intel(R) HD Graphics 4000Drv Version : 10.18.10.4425";
 	sendPacket(reportPkt, EPIC_LATEST);
 
 	TS_CS_CHARACTER_LIST charlistPkt;
