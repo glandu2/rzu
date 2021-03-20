0f00b718533d1d584da32ffaddadec1e02d547f9
glandu2 <glandu2epvp@gmail.com>
2021-03-20 22:21:38 +0100

    gitall: update submodules

--- gitall.sh
+++ gitall.sh
@@ -3,20 +3,9 @@
 
 echo ----libuv: && cd libuv && git "$@"
 echo ----libiconv: && cd ../libiconv && git "$@"
-echo ----librzu: && cd ../librzu && git "$@"
 echo ----liblua: && cd ../liblua && git "$@"
 echo ----gtest: && cd ../gtest && git "$@"
-echo ----rztest: && cd ../rztest && git "$@"
 echo ----zlib: && cd ../zlib && git "$@"
-echo ----rzauth: && cd ../rzauth && git "$@"
-echo ----rzbenchauth: && cd ../rzbenchauth && git "$@"
-echo ----rzgame: && cd ../rzgame && git "$@"
-echo ----rzgamereconnect: && cd ../rzgamereconnect && git "$@"
-echo ----rzchatgateway: && cd ../rzchatgateway && git "$@"
-echo ----rzplayercount: && cd ../rzplayercount && git "$@"
-echo ----rzlog: && cd ../rzlog && git "$@"
-echo ----rzfilter: && cd ../rzfilter && git "$@"
-echo ----rzauctionmonitor: && cd ../rzauctionmonitor && git "$@"
 
-echo ----rzu-parent: && cd .. && git "$@"
+echo ----rzu: && cd .. && git "$@"
 
